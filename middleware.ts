feee39f36bae3096a392a55b65d5a5d7bd983e53
akhilreddy-24 <21eg107a24@anurag.edu.in>
2024-07-10 17:14:21 +0530

    now

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,8 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
-
-export default clerkMiddleware((auth, request) => {
-  publicRoutes: ["/api/webhooks/clerk"];
-  if(!isPublicRoute(request)) {
-    auth().protect();
-  }
-});
+// Make sure that the `/api/webhooks/(.*)` route is not protected here
+export default clerkMiddleware()
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-}
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
